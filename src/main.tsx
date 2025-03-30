7944e1b1c2e05f4ef6bfe8cdc24f1a73741e1041
Carrotw <91509157+SantiHSilva@users.noreply.github.com>
2025-03-30 16:31:55 -0500

    reinstall

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
-import { render } from 'preact'
-import './index.css'
-import { App } from './app.tsx'
+import { createRoot } from 'react-dom/client'
+import App from './App'
 
-render(<App />, document.getElementById('app')!)
+createRoot(document.getElementById('root')!).render(
+    <App />
+)
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
-import preact from '@preact/preset-vite'
+import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [preact()],
+  plugins: [react()],
 })
