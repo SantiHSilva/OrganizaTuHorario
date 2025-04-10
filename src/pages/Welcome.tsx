1975761ba76378c1b0d4924e8ab63dd45f580c00
Carrotw <91509157+SantiHSilva@users.noreply.github.com>
2025-04-10 16:21:58 -0500

    add example

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -24,12 +24,21 @@ export default function Welcome() {
             ]}
             repeat={Infinity}
           />
-          <button
-            className="px-4 py-2 mt-4 w-full md:w-auto bg-blue-600 text-white rounded hover:bg-blue-700 transition cursor-pointer"
-            onClick={() => window.location.href = '/auth/login'}
-          >
-            Comenzar 🚀
-          </button>
+          <section className="flex flex-row items-center justify-start mt-4 gap-2">
+            <button
+              className="px-4 py-2 w-full md:w-auto bg-blue-600 text-white rounded hover:bg-blue-700 transition cursor-pointer"
+              onClick={() => window.location.href = '/auth/login'}
+            >
+              Comenzar 🚀
+            </button>
+            <a
+              href="https://santihsilva.github.io/OrganizaTuHorario/#/import/?groupList=NoCg5AhghAjAfgIymAsgSYCoHkAiWDKABAGICqAcjgIIoCi5GVAMrUfYQMJbn4YBKpDhwD8OWoXxZiGAOpU+tAHoBWAGwBOAMwATAEwASAHwAGAOzqALAGlVAUmMBFGBYBUVAO4xjMAES7EyLQsHBgAkgBqVIRihAAKfFIUOKEAWlQcoQDL5IoIAGYWMMp5Rs6a6mAAlAA0APoABrEATgD2eRIApgDmAK5NnE0AlgAOAC4thEyDAHY9AB5WFrbqDrowbu66xsoAEgBkAFJm6uqL9k7rHlum+1V1MA8wp7q2ABxO6gBeOJ7Guj6aAJgWJMKjkULEUIcdLCACahAA4qwMKJxAoxER4fhaHxwlDQgRFK9tBBTDAEPpyBw+AAuQhecyvM7KJymDZbXQ7QxlVRnd5bdl-HzKIEwADU2lMAGM1CUABoHMrKKwAHx2CoAvi4fltdgBuRUWTSmKwAHk2xlUOwA9AcAP4uABCFpuhAAOqMAI6Ksy881eGA7T0AXicr21v3+qiBujFeTyr2MSf0CqVqvVBy1Ou2OwNZRN5q2VttDud1x27q9PtMft+gZDYYjXl0YAAAq2XJ8AN47Ns1cAdKDAWoHO61QxWKoAXTgAAsh+BBsgHDSk2A4ABrZBy+ocFoAW2GABtooMAM4dACPEwRHTPozirQAnh0pbVwHkoKMmj0OnAusgnq0Eer6jIMABuECPi0XRNBA+4QFKgwAHSEB00xxE+oyzi00zuPgNCEJ8NLKBUcBHguYAAd+v5zsgPh9lO-aQFA+jrtolGDsOtjrgAVsgMB7I0rTtFQx6DFKUHaB0hCOhAF5ntEECjGKhAALSEEEtAhFCURcCgIK0HQDDyKEVCKNa9ShNM34tNoPRSlKhBQUeUF8C0CAtGBkl6sBoEQVB1ldOhgwdEMUG0FKuEHhJECmqofHIGYDjGOobjBmOXYPEamhWMotiqIY5B8BwdIwMIWTkIQraVExU6KOuUrIAAxPUflSg+Xb4BAR4AM-THSKowLYpgfJ8VxmBqZTWJoBVhnaRSFHlyyrDAACAdoaiqXb1IorbCIYwamnq1pinstj6D4nzuJ6tRyg4VgHDsLi1EAA"
+              target="_blank"
+              className="px-4 py-2 w-full md:w-auto bg-gray-200 text-gray-800 rounded hover:bg-gray-300 transition cursor-pointer"
+            >
+              Ver Ejemplo 📊
+            </a>
+          </section>
         </div>
         <section className="pb-10 md:pb-0">
           <img src="/demo_white.png" className="hidden md:block dark:hidden" />
