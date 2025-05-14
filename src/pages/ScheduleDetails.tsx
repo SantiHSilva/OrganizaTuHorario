8f4497e8ab3c8587f57558be94b6c95a9873e1f4
Carrotw <91509157+SantiHSilva@users.noreply.github.com>
2025-05-14 02:56:28 -0500

    Guardar horarios a la nube

--- src/pages/ScheduleDetails.tsx
+++ src/pages/ScheduleDetails.tsx
@@ -1,7 +1,39 @@
+import { useParams } from "react-router";
+import { API } from "../api/api";
 import HeaderBar from "../context/global/HeaderBar";
+import { getGroupList } from "../Data/groupManager";
 import HorarioProduction from "../src/HorarioProduction";
+import { toast } from "react-toastify";
 
 export default function ScheduleDetails(){
+
+  const {id} = useParams();
+
+  async function saveShcedules(){
+
+    if(!id){
+      toast.error("No se ha encontrado el ID del horario");
+    }
+
+    const groupList = getGroupList();
+    console.log("Group List: ", groupList);
+
+    const response = await API.post(`/HorariosUsuarios/save/${id}`, groupList)
+    .then((response) => {
+      console.log("Response: ", response);
+      return response;
+    })
+    .catch((error) => {
+      toast.error("Error guardando los cambios");
+      console.log("Error: ", error);
+    })
+
+    if(response?.status === 201){
+      toast.success("Cambios guardados correctamente");
+    }
+
+  }
+
   return (
     <div className='bg-white dark:bg-[#101218] flex flex-col'>
       <HeaderBar />
@@ -9,7 +41,10 @@ export default function ScheduleDetails(){
         <p className="text-2xl font-bold text-gray-800 dark:text-white">
           Área Administrativa
         </p>
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded cursor-pointer">
+        <button 
+          className="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded cursor-pointer"
+          onClick={saveShcedules}
+        >
           Guardar Cambios
         </button>
       </section>
